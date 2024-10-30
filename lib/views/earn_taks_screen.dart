39c8f9e3d6d038e867255ccc4886d73009e67396
IshaqFarid280 <IshaqFarid280>
2024-10-30 11:09:56 +0500

    abc

--- lib/views/earn_taks_screen.dart
+++ lib/views/earn_taks_screen.dart
@@ -102,10 +102,15 @@ $inviteLink
                       stream: FirebaseServices.getUserData(userId: userTelegramId.toString()),
                       builder: (context, snapshot) {
                         if (snapshot.connectionState == ConnectionState.waiting) {
-                          return SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.25,
-                            width: MediaQuery.of(context).size.width,
-                            child: CircularProgressIndicator(),
+                          return Center(
+                            child: SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              width: MediaQuery.of(context).size.width * 0.06,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 2.0,
+                                value: 10.0,
+                              ),
+                            ),
                           );
                         } else if (snapshot.hasData) {
                           var data = snapshot.data!.data() as Map<String, dynamic>;
@@ -125,83 +130,113 @@ $inviteLink
                                     'assets/karatekid1.png',
                                     fit: BoxFit.cover,
                                     opacity:  const AlwaysStoppedAnimation(.4),
-                                    height: MediaQuery.of(context).size.height * 0.3,
-                                    width: MediaQuery.of(context).size.width * 0.5,
+                                    height: MediaQuery.of(context).size.height * 0.25,
+                                    width: MediaQuery.of(context).size.width * 0.6,
                                   ),
                                 ),
                               ),
-                              Column(
+                              Positioned(
+                                top: 20,
+                                left: 15,
+
+                             child:  Row(
                                 children: [
-                                  Text(
-                                    'Welcome, ${controller.name.value}!',
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                  Sized(width: 0.04,),
+
+                                  Container(
+
+                                    decoration: BoxDecoration(
+
+                                        image: DecorationImage(image: AssetImage(
+                                            'assets/ninja.png'
+                                        )),
+                                        shape: BoxShape.circle,
+                                        color: whiteColor.withOpacity(0.8)
+                                    ),
+                                    width: MediaQuery.of(context).size.width*0.1,
+                                    height: MediaQuery.of(context).size.height*0.05,
                                   ),
-
-                                  Sized(height: 0.06,),
-
-                                  Row(
-                                                      mainAxisAlignment:  MainAxisAlignment.center,
+                                  Column(
                                     children: [
-                                      InkWell(
-                                        onTap: (){
-                                          print(controller.name.value);
-                                          print(controller.userId.value);
-                                          var encodedUsername =
-                                          Uri.encodeComponent(controller.name.value);
-                                          final inviteLink =
-                                              'https://telegrambot-dbb20.web.app/?referrerId=${controller.userId.value}&username=$encodedUsername';
-                                          controller.copyToClipboard(inviteLink, context);
+                                      mediumText(title:
+                                      '${controller.name.value}',
+                                          fontSize: 20.0
+                                      ),
+                                      smallText(title: 'the id', fontSize: 14.0, color: whiteColor.withOpacity(0.7)),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              ),
+                              Positioned(
+                                  top: 50,
+                                  left: 5,
+                                child:
+                                Row(
+                                  mainAxisAlignment:  MainAxisAlignment.center,
 
-                                          // Option 1: Open Telegram share link
-                                          shareInviteLink(inviteLink);
+                                  children: [
 
-                                          // Option 2: Send the invite message to Telegram using the bot
-                                          // sendTelegramInvite(controller.userId.value, inviteLink, 'YOUR_BOT_TOKEN_HERE');
-                                        },
-                                        child: Container(
-                                          width: MediaQuery.of(context).size.width*0.7,
-                                          height: MediaQuery.of(context).size.height*0.05,
+                                    InkWell(
+                                      onTap: (){
+                                        print(controller.name.value);
+                                        print(controller.userId.value);
+                                        var encodedUsername =
+                                        Uri.encodeComponent(controller.name.value);
+                                        final inviteLink =
+                                            'https://telegrambot-dbb20.web.app/?referrerId=${controller.userId.value}&username=$encodedUsername';
+                                        controller.copyToClipboard(inviteLink, context);
 
-                                          child: Row(
-                                            mainAxisAlignment:  MainAxisAlignment.center,
-                                            children: [
-                                              mediumText(title: 'Send Invite'.toUpperCase(), fontSize: 22.0),
-                                              Sized(width: 0.019,),
-                                              Icon(Icons.send)
+                                        // Option 1: Open Telegram share link
+                                        shareInviteLink(inviteLink);
 
-                                            ],
-                                          ),
-                                          decoration: BoxDecoration(
+                                        // Option 2: Send the invite message to Telegram using the bot
+                                        // sendTelegramInvite(controller.userId.value, inviteLink, 'YOUR_BOT_TOKEN_HERE');
+                                      },
+                                      child: Container(
+                                        width: MediaQuery.of(context).size.width*0.7,
+                                        height: MediaQuery.of(context).size.height*0.05,
+
+                                        child: Row(
+                                          mainAxisAlignment:  MainAxisAlignment.center,
+                                          children: [
+                                            mediumText(title: 'Send Invite'.toUpperCase(), fontSize: 22.0),
+                                            Sized(width: 0.019,),
+                                            Icon(Icons.send)
+
+                                          ],
+                                        ),
+                                        decoration: BoxDecoration(
                                             borderRadius: BorderRadius.circular(8),
                                             color: whiteColor.withOpacity(0.2)
-                                          ),
                                         ),
                                       ),
-                                      Sized(
-                                        width: 0.025,
-                                      ),
-                                      InkWell(
-                                        onTap: (){
-                                                      var encodedUsername =
-                                                      Uri.encodeComponent(controller.name.value);
-                                                      final inviteLink =
-                                                      'https://telegrambot-dbb20.web.app/?referrerId=${controller.userId.value}&username=$encodedUsername';
-                                                      controller.copyToClipboard(inviteLink, context);
-                                                      },
-                                        child: Container(
+                                    ),
+                                    Sized(
+                                      width: 0.025,
+                                    ),
+                                    InkWell(
+                                      onTap: (){
+                                        var encodedUsername =
+                                        Uri.encodeComponent(controller.name.value);
+                                        final inviteLink =
+                                            'https://telegrambot-dbb20.web.app/?referrerId=${controller.userId.value}&username=$encodedUsername';
+                                        controller.copyToClipboard(inviteLink, context);
+                                      },
+                                      child: Container(
                                         padding: EdgeInsets.all(8),
-                                            child: Icon(Icons.link_rounded, color: whiteColor , size: 36,),
+                                        child: Icon(Icons.link_rounded, color: whiteColor , size: 36,),
 
                                         decoration: BoxDecoration(
                                             color: whiteColor.withOpacity(0.2),
-                                          shape: BoxShape.circle
+                                            shape: BoxShape.circle
                                         ),
-                                        ),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
+                                      ),
+                                    ),
+
+                                  ],
+                                )
+                              )
 
 
 
